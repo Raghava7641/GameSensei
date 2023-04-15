0c161f4615241ecdbe6267cb4c19cd29f23f954c
Hari Galla <admin@MacBook-Air-238.local>
2023-04-15 15:15:45 -0400

    Added env variables for db credentials and changed the cors policy in express

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,28 @@
 const mongoose = require('mongoose')
-// const GameModel = require('./models/gameModel')
 const express = require('express')
-const cors = require('cors')
+require('dotenv').config()
 
+const cors = require('cors')
 const router = require('./api/routes')
 
 const app = express()
 
 app.use(express.json())
 
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@myfirstcluster0.q1bejuz.mongodb.net/${process.env.DB_NAME}`,
+)
+
+whiteListPorts = ['http://127.0.0.1:3005', 'http://localhost:3005']
+
 corsOptions = {
-  origin: 'http://127.0.0.1:3001',
+  origin: (origin, callback) => {
+    if (whiteListPorts.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
   methods: 'GET,PUT,POST',
   allowedHeaders: 'Content-Type,Authorization',
 }
@@ -18,10 +30,6 @@ corsOptions = {
 app.use(cors(corsOptions))
 
 app.use('/', router)
-
-mongoose.connect(
-  'mongodb+srv://hgalla0426:FuRuvmnpd20PA7eq@myfirstcluster0.q1bejuz.mongodb.net/TaHaRa_games_db',
-)
 
 app.listen(7077, () => {
   console.log(`Express server running on port ${7077}`)
@@ -33,12 +41,4 @@ app.listen(7077, () => {
 //     test: String,
 //   },
 //   { collection: 'demo' },
-// )
-
-// const testModel = mongoose.model('demo', testSchema)
-
-// testModel.find({}).then((e) => console.log('data fetched', e))
-
-// GameModel.find({ Name: 'Grand Theft Auto V' }).then((data) =>
-//   console.log('data fetched', data),
 // )
