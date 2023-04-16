a09e3af54ff05f24ca635d0ab288bf5803028687
Hari Galla <admin@MacBook-Air-238.local>
2023-04-15 20:09:11 -0400

    Big Change: Added middleware code to find the images for games, cleaned up on the FE, and integrated the service call with material ui cards

--- backend/api/routes.js
+++ backend/api/routes.js
@@ -1,16 +1,60 @@
 const express = require('express')
 const GameModel = require('../models/gameModel')
+const axios = require('axios')
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  const response = await GameModel.find({ Name: 'Grand Theft Auto V' })
+  const response = await GameModel.find({}).sort({ Critic_Score: -1 }).limit(20)
+  const updatedResponse = await appendGameThumbnail(response)
 
-  res.send(response)
+  res.send(updatedResponse)
 })
 
 router.get('/test', (req, res) => {
   res.send('Hello World')
 })
+
+const appendGameThumbnail = async (dbResponse) => {
+  const updatedResponse = await Promise.all(
+    dbResponse.map(
+      async ({ _id, Name, Platform, Year_of_Release, Genre, Critic_Score }) => {
+        imageLink = 'No Image Available!'
+
+        if (Name.trim()) {
+          const { data } = await axios.get(
+            'http://www.gamespot.com/api/games',
+            {
+              params: {
+                api_key: '502f699ca28dbbc8f9a6a081596739b63ec93507',
+                format: 'json',
+                field_list: 'name,image',
+                filter: `name:${Name}`,
+              },
+            },
+          )
+
+          if (data?.results.length > 0) {
+            if (data.results[0]?.image?.square_tiny) {
+              imageLink = data.results[0].image.square_tiny
+            }
+          }
+        }
+
+        return {
+          _id,
+          Name,
+          Platform,
+          Year_of_Release,
+          Genre,
+          Critic_Score,
+          imageLink,
+        }
+      },
+    ),
+  )
+
+  return updatedResponse
+}
 
 module.exports = router
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+  Rating,
+  Box,
+  Container,
+  Grid,
+  Skeleton,
+} from '@mui/material'
 
 import { sampleApiCall } from './services/gamesApi'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   const [games, setGames] = useState([])
 
   useEffect(() => {
@@ -20,34 +29,66 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <p> Sample Api call result: </p>
-        <ol>
-          {games.map((game) => (
-            <li key={game?._id}>
-              {game?.Name} -- {game?.Platform}
-            </li>
-          ))}
-        </ol>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container>
+        <Typography variant="h1" component="h1" my={2}>
+          Game Sensei
+        </Typography>
+        <Box>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
+            {games.length !== 0 ? (
+              games.map((game) => (
+                <Grid item xs={12} sm={6} md={6} lg={4} key={game?._id}>
+                  <Card sx={{ minHeight: 340 }}>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image={game?.imageLink}
+                        alt={game?.Name}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {game?.Name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          Year: {game?.Year_of_Release}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          Rating: {game?.Critic_Score / 10}
+                        </Typography>
+                        <Rating
+                          name="customized-10"
+                          value={game?.Critic_Score / 10}
+                          readOnly
+                          max={10}
+                        />
+                        <Typography variant="body2" color="text.secondary">
+                          Platform: {game.Platform}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+              ))
+            ) : (
+              <Grid container spacing={3}>
+                {Array.from(Array(20)).map((_, index) => (
+                  <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                    <Skeleton variant="rectangular" height={250} />
+                    <Skeleton />
+                    <Skeleton />
+                    <Skeleton />
+                  </Grid>
+                ))}
+              </Grid>
+            )}
+          </Grid>
+        </Box>
+      </Container>
     </div>
   )
 }
